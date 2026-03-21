d5667b14f2eb814dbfd3b59bf1fecdad8b398e30
Wasp <237859955+raprapyap1000-ux@users.noreply.github.com>
2026-03-21 18:49:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from getpass import getpass
 
 UserAgents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
-    # ... [rest of user agents] ...
 ]
 
 ascii_art = """
